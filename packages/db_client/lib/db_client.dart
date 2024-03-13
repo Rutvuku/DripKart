514150e6ebc76061c82befda09ef3cab14336a8d
rutvuku <rutvikakarsh69@gmail.com>
2024-03-14 04:01:45 +0530

    Completed Firebase Setup

---
+++ packages/db_client/lib/db_client.dart
@@ -0,0 +1,5 @@
+library db_client;
+
+/// A Calculator.
+export 'src/db_client.dart';
+export 'src/db_record.dart';
---
+++ packages/db_client/lib/src/db_record.dart
@@ -0,0 +1,5 @@
+class DbRecord{
+  final String id;
+  final Map<String,dynamic> data;
+  DbRecord({required this.id,this.data=const {}});
+}
