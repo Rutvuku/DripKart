57a53b1767dfa4756c23b254711f6143fbcd0e8e
rutvuku <rutvikakarsh69@gmail.com>
2024-03-29 00:21:52 +0530

    start of video conferencing app

--- packages/db_client/lib/db_client.dart
+++
@@ -1,5 +0,0 @@
-library db_client;
-
-/// A Calculator.
-export 'src/db_client.dart';
-export 'src/db_record.dart';
--- packages/db_client/lib/src/db_record.dart
+++
@@ -1,5 +0,0 @@
-class DbRecord{
-  final String id;
-  final Map<String,dynamic> data;
-  DbRecord({required this.id,this.data=const {}});
-}
